3aa85ee05e8b1c507feed018703bb17bca087ec8
Sam Rijs <srijs@airpost.net>
2020-04-11 10:44:56 +0200

    improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,34 @@
 //! The GEAR hashing function is a fast, rolling hash function that
-//! is well suited for content defined chunking.
+//! is well suited for content defined chunking. In particular, it is
+//! used as a building block for the [FastCDC](https://www.usenix.org/node/196197)
+//! algorithm.
 //!
-//! In particular, this function is used as a building block for the
-//! [FastCDC](https://www.usenix.org/node/196197) algorithm.
-//
 //! The implementation provided in this crate consists of both a simple,
 //! scalar variant, as well as optimized versions for the SSE4.2 and AVX2
 //! instruction sets.
+//!
+//! ## Example
+//!
+//! ```
+//! fn find_all_chunks(buf: &[u8], mask: u64) -> Vec<&[u8]> {
+//!     // set up initial state
+//!     let mut chunks = vec![];
+//!     let mut offset = 0;
+//!
+//!     // create new hasher
+//!     let mut hasher = gearhash::Hasher::default();
+//!
+//!     // loop through all matches, and push the corresponding chunks
+//!     while let Some(boundary) = hasher.next_match(&buf[offset..], mask) {
+//!         chunks.push(&buf[offset..offset + boundary]);
+//!         offset += boundary;
+//!     }
+//!
+//!     // push final chunk
+//!     chunks.push(&buf[offset..]);
+//!     chunks
+//! }
+//! ```
 
 #![cfg_attr(feature = "bench", feature(test))]
 
@@ -24,7 +46,7 @@ pub mod fuzzing;
 
 pub use table::{Table, DEFAULT_TABLE};
 
-/// Gear hash state.
+/// Gear hash state. Processes bytes to find chunk boundaries.
 #[derive(Clone)]
 pub struct Hasher<'t> {
     table: &'t Table,
